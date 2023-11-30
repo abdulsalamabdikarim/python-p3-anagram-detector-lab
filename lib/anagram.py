5a38f4bca951f64e7cd0815317e42a16a9e3e040
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-30 23:33:20 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        anagram_match = [element for element in list if sorted(element) == sorted(self.word)]
+        return anagram_match
+        # for element in list:
+        #     if sorted(element) == sorted(self.word):
+        #         anagram_match.append(element)
+        #     else:
+        #         return anagram_match
+        # print(anagram_match)
+
+y = Anagram("race")
+g = y.match(['enlist', 'google', "silent", 'care','reca', "reac"])
+print(g)
+
